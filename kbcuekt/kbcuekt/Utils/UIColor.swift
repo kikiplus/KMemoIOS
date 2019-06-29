b704f6f67630ba7f6d39c9fd60b1a6aeaf61d4ce
grapegirl <kikiplus2030@gmail.com>
2019-06-29 12:04:02 +0900

    10st
    Realm Swift 5.0

--- kbcuekt/kbcuekt/Utils/UIColor.swift
+++ kbcuekt/kbcuekt/Utils/UIColor.swift
@@ -42,4 +42,14 @@ extension UIColor {
         )
         return color
     }
+    
+    //16진수 색상값을 UIColor 객체로 변환되는 함수
+    func UIClorFromRGB(rgbValue : UInt ) -> UIColor {
+        return UIColor(
+            red : CGFloat((rgbValue & 0xFF0000) >> 16 ) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0 ,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
 }
